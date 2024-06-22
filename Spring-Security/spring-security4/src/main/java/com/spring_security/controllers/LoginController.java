bce27f7ea83e444cd181ed9c0bea0f29ad54feb5
Rishi Singh <cs24rishi@gmail.com>
2024-06-23 02:05:06 +0530

    configured bcypt for hasing password

--- Spring-Security/spring-security4/src/main/java/com/spring_security/controllers/LoginController.java
+++ Spring-Security/spring-security4/src/main/java/com/spring_security/controllers/LoginController.java
@@ -5,6 +5,7 @@ import com.spring_security.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,16 @@ public class LoginController {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    PasswordEncoder encoder;
+
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity response = null;
-
         try {
+            String hashedPwd = encoder.encode(customer.getPwd());
+            customer.setPwd(hashedPwd);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity.status(HttpStatus.OK)
