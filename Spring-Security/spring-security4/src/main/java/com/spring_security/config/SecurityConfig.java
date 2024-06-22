af6aeb808479099af4882a3ab1e6fd6fbdf19d14
Rishi Singh <cs24rishi@gmail.com>
2024-06-23 01:43:49 +0530

    removed commented code

--- Spring-Security/spring-security4/src/main/java/com/spring_security/config/SecurityConfig.java
+++ Spring-Security/spring-security4/src/main/java/com/spring_security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -31,48 +32,6 @@ public class SecurityConfig {
 
         return (SecurityFilterChain) http.build();
     }
-
-
-//    /**
-//     * Approch 1 use withDefaultPasswordEncoder
-//     */
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService(){
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//            .username("admin")
-//            .password("admin")
-//            .authorities("admin")
-//            .build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//            .username("user")
-//            .password("user")
-//            .authorities("read")
-//            .build();
-//
-//        return new InMemoryUserDetailsManager(admin,user);
-//    }
-
-    /**
-     * Approch 2 explicitly defining the password encoder
-     */
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//
-//        UserDetails admin =
-//            User.withUsername("admin").password("admin").authorities("admin").build();
-//        UserDetails user = User.withUsername("user").password("user").authorities("read").build();
-//
-//        manager.createUser(admin);
-//        manager.createUser(user);
-//
-//        return manager;
-//    }
-
-//    @Bean
-//    public JdbcUserDetailsManager userDetailsService(DataSource dataSource){
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
