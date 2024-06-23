154bfbfdb9984f4a0d4dbbd96ca8867b352b6c54
Rishi Singh <cs24rishi@gmail.com>
2024-06-24 01:36:12 +0530

    added cors configuration

--- Spring-Security/spring-security6/src/main/java/com/spring_security/config/SecurityConfig.java
+++ Spring-Security/spring-security6/src/main/java/com/spring_security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring_security.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,8 +13,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -21,6 +25,18 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }
+            }))
             .csrf((csrf) -> csrf.disable())
             .authorizeHttpRequests((requests) -> {
             requests
