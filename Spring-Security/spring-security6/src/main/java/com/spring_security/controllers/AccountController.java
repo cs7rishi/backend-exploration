f0f61458b4aa36a67eacd70805b61f21f08d8aa7
Rishi Singh <cs24rishi@gmail.com>
2024-06-23 17:27:33 +0530

    updated controllers

--- Spring-Security/spring-security6/src/main/java/com/spring_security/controllers/AccountController.java
+++ Spring-Security/spring-security6/src/main/java/com/spring_security/controllers/AccountController.java
@@ -1,13 +1,26 @@
 package com.spring_security.controllers;
+
+import com.spring_security.entities.Accounts;
+import com.spring_security.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
-    @GetMapping("/myaccount")
-    public String getAccountDetails() {
-        return "Here are the account details from the DB";
+    @Autowired
+    private AccountsRepository accountsRepository;
+
+    @GetMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null ) {
+            return accounts;
+        }else {
+            return null;
+        }
     }
 
 }
--- Spring-Security/spring-security6/src/main/java/com/spring_security/controllers/NoticesController.java
+++ Spring-Security/spring-security6/src/main/java/com/spring_security/controllers/NoticesController.java
@@ -1,13 +1,33 @@
 package com.spring_security.controllers;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.spring_security.entities.Notice;
+import com.spring_security.repository.NoticeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class NoticesController {
 
+    @Autowired
+    private NoticeRepository noticeRepository;
+
     @GetMapping("/notices")
-    public String getNotices() {
-        return "Here are the notices details from the DB";
+    public ResponseEntity<List<Notice>> getNotices() {
+        List<Notice> notices = noticeRepository.findAllActiveNotices();
+        if (notices != null ) {
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                    .body(notices);
+        }else {
+            return null;
+        }
     }
 
 }
