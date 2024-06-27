6873d6e2c88dcd4acaa9915ea9a8b5ee3f6cc71f
Rishi Singh <cs24rishi@gmail.com>
2024-06-28 02:59:42 +0530

    inmplemented csrf protection in the UI applicaiton

--- Spring-Security/bank-app-ui/src/app/constants/app.constants.ts
+++ Spring-Security/bank-app-ui/src/app/constants/app.constants.ts
@@ -2,9 +2,9 @@
 export const  AppConstants = {
     CONTACT_API_URL : "/contact",
     LOGIN_API_URL : "/user",
-    ACCOUNT_API_URL : "/myAccount",
-    BALANCE_API_URL : "/myBalance",
-    LOANS_API_URL : "/myLoans",
-    CARDS_API_URL : "/myCards",
+    ACCOUNT_API_URL : "/myaccount",
+    BALANCE_API_URL : "/mybalance",
+    LOANS_API_URL : "/myloans",
+    CARDS_API_URL : "/mycards",
     NOTICES_API_URL : "/notices"
 }
