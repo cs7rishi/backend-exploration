3d8daab07db53c009a3729fd3ae4015387493070
Rishi Singh <cs24rishi@gmail.com>
2024-06-30 02:06:53 +0530

    initialized project for method level security exploration

---
+++ Spring-Security/spring-security10/backend/src/main/java/com/spring_security/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ Spring-Security/spring-security10/backend/src/main/java/com/spring_security/controllers/CardsController.java
@@ -0,0 +1,28 @@
+package com.spring_security.controllers;
+
+import com.spring_security.entities.Cards;
+import com.spring_security.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @GetMapping("/mycards")
+    public List<Cards> getCardDetails(@RequestParam int id) {
+        List<Cards> cards = cardsRepository.findByCustomerId(id);
+        if (cards != null ) {
+            return cards;
+        }else {
+            return null;
+        }
+    }
+
+}
