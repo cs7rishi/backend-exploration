9e5370fd1a37311fb85720b95e2957c6c0b4395e
Rishi Singh <cs24rishi@gmail.com>
2024-09-08 01:04:55 +0530

    explored buffer file reader

--- Java/File IO/classicIO/characterStream/FileReaderWriter.java
+++ Java/File IO/classicIO/characterStream/FileReaderWriter.java
@@ -3,6 +3,10 @@ package characterStream;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * Example of Read/Write from file
+ * Used Buffer reading for performance optimization
+ */
 public class FileReaderWriter {
     private static final String dir =
         "D:\\Learning\\GitHub\\backend-exploration\\Java\\File IO\\resources\\characterStream";
@@ -11,13 +15,35 @@ public class FileReaderWriter {
 
     public static void main(String[] args) throws IOException {
         createResource();
-//        readerWriterExample();
-        readerWriterWithTryWithResources();
+        //readerWriterExample();
+        //readerWriterWithTryWithResources();
+        readerWriterUsingBuffer();
     }
 
+    /**
+     * This method utilized buffer array for reading/writing
+     * The buffer is not fully utilized in each iteration
+     */
+    private static void readerWriterUsingBuffer() {
+        try (Reader reader = new FileReader(inputFile, StandardCharsets.UTF_8);
+            Writer writer = new FileWriter(outputFile, StandardCharsets.UTF_8);) {
+
+            char[] buffer = new char[1024];
+
+            while(reader.read(buffer) != -1){
+                writer.write(buffer);
+            }
+        } catch (IOException ex) {
+            System.out.println((ex.getMessage()));
+            System.out.println("Exception occured while reading/writing");
+        }
+    }
+
+    /**
+     * This method using same read/write with try with resources, reducing verbosity.
+     */
     private static void readerWriterWithTryWithResources() {
-        try (
-            Reader reader = new FileReader(inputFile, StandardCharsets.UTF_8);
+        try (Reader reader = new FileReader(inputFile, StandardCharsets.UTF_8);
             Writer writer = new FileWriter(outputFile);) {
             int charRead = 0;
             while ((charRead = reader.read()) != -1) {
@@ -29,6 +55,9 @@ public class FileReaderWriter {
         }
     }
 
+    /**
+     * This method uses Reader/Writer for reading byte by byte
+     */
     private static void readerWriterExample() {
         //InputStreamReader reader = new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8);
         Reader reader = null;
@@ -57,8 +86,9 @@ public class FileReaderWriter {
     private static void createResource() {
         try {
             outputFile.delete();
+            Thread.sleep(5000);
             outputFile.createNewFile();
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
